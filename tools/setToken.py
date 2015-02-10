d7f2159048679a520e6c5565d2886897f0639505
Eric Koldinger <kolding@washington.edu>
2015-02-09 19:31:34 -0800

    Switched Util.getDefault to Defaults.getDefault.

--- tools/setToken.py
+++ tools/setToken.py
@@ -1,15 +1,15 @@
 #! /usr/bin/python
 
-from Tardis import Util, TardisDB, TardisCrypto
+from Tardis import Defaults, Util, TardisDB, TardisCrypto
 import os.path
 import logging
 import argparse
 
 def processArgs():
     parser = argparse.ArgumentParser(description='Set a token/password')
-    parser.add_argument('--database', '-D', dest='database', default=Util.getDefault('TARDIS_DB'),      help="Database to use.  Default: %(default)s")
-    parser.add_argument('--client', '-C',   dest='client',   default=Util.getDefault('TARDIS_CLIENT'),  help="Client to list on.  Default: %(default)s")
-    parser.add_argument('--dbname',         dest='dbname',   default=Util.getDefault('TARDIS_DBNAME'),  help="Name of the database file. Default: %(default)s")
+    parser.add_argument('--database', '-D', dest='database', default=Defaults.getDefault('TARDIS_DB'),      help="Database to use.  Default: %(default)s")
+    parser.add_argument('--client', '-C',   dest='client',   default=Defaults.getDefault('TARDIS_CLIENT'),  help="Client to list on.  Default: %(default)s")
+    parser.add_argument('--dbname',         dest='dbname',   default=Defaults.getDefault('TARDIS_DBNAME'),  help="Name of the database file. Default: %(default)s")
 
     passgroup= parser.add_argument_group("Password/Encryption specification options")
     pwgroup = passgroup.add_mutually_exclusive_group(required=True)
