f54587cd66177707ad447dc9ab6a7ec974db8a25
Eric Koldinger <kolding@washington.edu>
2015-02-12 12:23:18 -0800

    Added a progress bar to the convert routine.

--- schema/convert-2to3.py
+++ schema/convert-2to3.py
@@ -2,6 +2,11 @@ import sqlite3
 import sys
 import os.path
 from Tardis import CacheDir
+try:
+    import progressbar
+    progress = True
+except Exception:
+    progress=False
 
 
 if len(sys.argv) > 1:
@@ -25,13 +30,32 @@ conn.execute('INSERT OR REPLACE INTO Config (Key, Value) VALUES ("SchemaVersion"
 
 cache = CacheDir.CacheDir(os.path.dirname(db))
 
+c = conn.execute("SELECT COUNT(*) FROM Checksums WHERE DiskSize IS NULL")
+r = c.fetchone()
+numrows = r[0]
+print numrows
+
 c = conn.execute("SELECT Checksum FROM Checksums WHERE DiskSize IS NULL")
 checksums = c.fetchall()
+
+# Build a progress bar, if we have that module.  Just for grins.
+if progress:
+    widgets = [ progressbar.Counter(), ' ', progressbar.Bar(), ' ', progressbar.ETA() ]
+    pbar = progressbar.ProgressBar(widgets=widgets, maxval=numrows)
+    pbar.start()
+
 c2 = conn.cursor()
+x = 0
 for i in checksums:
     checksum = i[0]
     size = os.path.getsize(cache.path(checksum))
-    print "Setting size of %s to %d" % (checksum, size)
+    #print "Setting size of %s to %d" % (checksum, size)
     c2.execute("UPDATE Checksums SET DiskSize = ? WHERE Checksum = ?", (size, checksum))
+    x += 1
+    if progress:
+        pbar.update(x)
+
+if progress:
+    pbar.finish()
 
 conn.commit()
