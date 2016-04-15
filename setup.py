782512842080a772b9fedcd35ff8d894a3dea7ee
Eric Koldinger <kolding@washington.edu>
2016-04-15 00:00:42 -0700

    Added logwatch scripts.

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 from setuptools import setup, find_packages
 import sys, os
 import subprocess
+
+longdesc = '''
+This is a system for performing backups, supporting incremental, delta backups, with option encryption, and 
+recovery of data via either a filesystem based interface, or via explicit tools.  Please pardon any Dr. Who
+jokes.
+'''
     
 buildVersion = subprocess.check_output(['git', 'describe', '--dirty', '--tags', '--always']).strip()
 file('tardisversion', 'w').write(buildVersion + "\n")
@@ -17,18 +23,28 @@ add_pkgs = Tardis.__check_features()
 setup(  name                    = 'Tardis-Backup',
         version                 = version,
         description             = "Tardis Backup System",
+        long_description        = longdesc,
         packages                = find_packages(exclude=['ez_setup', 'examples', 'tests']),
+        author                  = "Eric Koldinger",
+        author_email            = "kolding@washington.edu",
+        url                     = "https://github.com/koldinger/Tardis",
+        license                 = "MIT",
+        platforms               = "Posix; MacOS X",
         include_package_data    = True,
         zip_safe                = False,
-        install_requires = ['msgpack-python', 'daemonize', 'parsedatetime', 'pycryptodomex', 'requests', 'requests_cache', 'flask', 'tornado', 'termcolor', 'python-magic' ] + add_pkgs,
-        data_files = [( root + '/etc/tardis',              [ 'tardisd.cfg', 'types.ignore', 'tardisremote.cfg' ]),
-                      ( 'schema',                          [ 'schema/tardis.sql' ]),
-                      ( 'info',                            [ 'tardisversion' ]),
-                      ( root + '/etc/init.d',              [ 'init/tardisd', 'init/tardisremote' ]),
-                      ( root + '/usr/lib/systemd/system',  [ 'init/tardisd.service', 'init/tardisremote.service' ]),
-                      ( root + '/etc/logrotate.d',         [ 'logrotate/tardisd', 'logrotate/tardisremote' ]),
-                      #( '/etc/logwatch/conf/services', [ 'logwatch/tardisd.conf' ]),
-                      #( '/etc/logwatch/scripts/services', [ 'logwatch/tardisd.pl' ]),
+        install_requires = ['msgpack-python', 'daemonize', 'parsedatetime', 'pycryptodomex', 'requests',
+                            'requests_cache', 'flask',     'tornado', 'termcolor',
+                            'python-magic' ] + add_pkgs,
+        data_files = [( root + '/etc/tardis',                     [ 'tardisd.cfg', 'types.ignore', 'tardisremote.cfg' ]),
+                      ( 'schema',                                 [ 'schema/tardis.sql' ]),
+                      ( 'info',                                   [ 'tardisversion' ]),
+                      ( root + '/etc/init.d',                     [ 'init/tardisd', 'init/tardisremote' ]),
+                      ( root + '/usr/lib/systemd/system',         [ 'init/tardisd.service', 'init/tardisremote.service' ]),
+                      ( root + '/etc/logrotate.d',                [ 'logrotate/tardisd', 'logrotate/tardisremote' ]),
+                      ( root + '/etc/logwatch/conf/services',     [ 'logwatch/conf/services/tardisd.conf' ]),
+                      ( root + '/etc/logwatch/conf/services',     [ 'logwatch/conf/services/tardisd.conf' ]),
+                      ( root + '/etc/logwatch/conf/logfiles',     [ 'logwatch/conf/logfiles/tardisd.conf' ]),
+                      ( root + '/etc/logwatch/scripts/services',  [ 'logwatch/scripts/services/tardisd' ]),
                      ],
         entry_points = {
                 'console_scripts' : [
