afef5fb8e334ef68ed922056c5af06e632bcfdba
Eric Koldinger <kolding@washington.edu>
2016-05-19 20:45:33 -0700

    Added some statistics to the Backups table.

---
+++ schema/convert-7to8.py
@@ -0,0 +1,27 @@
+import sqlite3
+import sys
+import os.path
+from Tardis import CacheDir
+
+version = 7
+
+if len(sys.argv) > 1:
+    db = sys.argv[1]
+else:
+    db = "tardis.db"
+
+conn = sqlite3.connect(db)
+
+s = conn.execute('SELECT Value FROM Config WHERE Key = "SchemaVersion"')
+t = s.fetchone()
+if int(t[0]) != version:
+    print("Invalid database schema version: {}".format(t[0]))
+    sys.exit(1)
+
+conn.execute("ALTER TABLE Backups ADD COLUMN FilesFull INTEGER")
+conn.execute("ALTER TABLE Backups ADD COLUMN FilesData INTEGER")
+conn.execute("ALTER TABLE Backups ADD COLUMN BytesReceived INTEGER")
+
+conn.execute('INSERT OR REPLACE INTO Config (Key, Value) VALUES ("SchemaVersion", ?)', str(version + 1))
+
+conn.commit()
