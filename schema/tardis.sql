983b752745ae7242e81ee69019f3f4f7a36c8ed5
Eric Koldinger <kolding@washington.edu>
2016-09-08 13:02:23 -0700

    Convert CHARACTER fields to TEXT fields.  Because SQLite doesn't bother to check any type names.   No functional changes.

--- schema/tardis.sql
+++ schema/tardis.sql
@@ -1,28 +1,28 @@
 CREATE TABLE IF NOT EXISTS Config (
-    Key             CHARACTER PRIMARY KEY,
-    Value           CHARACTER NOT NULL
+    Key             TEXT PRIMARY KEY,
+    Value           TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Backups (
-    Name            CHARACTER UNIQUE,
+    Name            TEXT UNIQUE,
     BackupSet       INTEGER PRIMARY KEY AUTOINCREMENT,
-    StartTime       CHARACTER,
-    EndTime         CHARACTER,
-    ClientTime      CHARACTER,
-    Session         CHARACTER UNIQUE,
+    StartTime       TEXT,
+    EndTime         TEXT,
+    ClientTime      TEXT,
+    Session         TEXT UNIQUE,
     Completed       INTEGER DEFAULT 0,
     Priority        INTEGER DEFAULT 1,
     Full            INTEGER DEFAULT 0,
-    ClientVersion   CHARACTER,
-    ServerVersion   CHARACTER,
-    ClientIP        CHARACTER,
+    ClientVersion   TEXT,
+    ServerVersion   TEXT,
+    ClientIP        TEXT,
     FilesFull       INTEGER,
     FilesDelta      INTEGER,
     BytesReceived   INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS CheckSums (
-    Checksum    CHARACTER UNIQUE NOT NULL,
+    Checksum    TEXT UNIQUE NOT NULL,
     ChecksumId  INTEGER PRIMARY KEY AUTOINCREMENT,
     Size        INTEGER,
     Basis       INTEGER,
@@ -37,18 +37,18 @@ CREATE TABLE IF NOT EXISTS CheckSums (
 );
 
 CREATE TABLE IF NOT EXISTS Names (
-    Name        CHARACTER UNIQUE NOT NULL,
+    Name        TEXT UNIQUE NOT NULL,
     NameId      INTEGER PRIMARY KEY AUTOINCREMENT
 );
 
 CREATE TABLE IF NOT EXISTS Files (
-    NameId      INTEGER   NOT NULL,
-    FirstSet    INTEGER   NOT NULL,
-    LastSet     INTEGER   NOT NULL,
-    Inode       INTEGER   NOT NULL,
-    Device      INTEGER   NOT NULL,
-    Parent      INTEGER   NOT NULL,
-    ParentDev   INTEGER   NOT NULL,
+    NameId      INTEGER  NOT NULL,
+    FirstSet    INTEGER  NOT NULL,
+    LastSet     INTEGER  NOT NULL,
+    Inode       INTEGER  NOT NULL,
+    Device      INTEGER  NOT NULL,
+    Parent      INTEGER  NOT NULL,
+    ParentDev   INTEGER  NOT NULL,
     ChecksumId  INTEGER,            -- On a file, represents the file data.  On a directory, the hash of the filenames in the directory.
                                     -- On a directory, this can be rewritten over time, and is the most recent hash.
     Dir         INTEGER,
