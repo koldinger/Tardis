892e04d05b9574a8c8b158bb8bc9290879c0949c
Eric Koldinger <kolding@washington.edu>
2017-06-28 11:33:08 -0700

    Mostly functioning version of encryptDB.  Still needs to generate metadata files, and remove old vestigal files.

--- tools/encryptDB.py
+++ tools/encryptDB.py
@@ -1,11 +1,12 @@
 #! /usr/bin/python
 
-from Tardis import Defaults, Util, TardisDB, TardisCrypto, CacheDir, librsync, Regenerate
+from Tardis import Defaults, Util, TardisDB, TardisCrypto, CacheDir, librsync, Regenerator
 import sqlite3
 import argparse, logging
 import os.path
 import os
 import base64
+import hashlib
 
 def encryptFilenames(db, crypto):
     conn = db.conn
@@ -22,50 +23,103 @@ def encryptFilenames(db, crypto):
 
     conn.commit()
 
-def encryptFile(checksum, cacheDir, cipher, iv, pad):
+def encryptFile(checksum, cacheDir, cipher, iv, pad, hmac, nameHmac):
     f = cacheDir.open(checksum, 'rb')
     o = cacheDir.open(checksum + '.enc', 'wb')
     o.write(iv)
-    for chunk, eof in Util.chunks(f, 64 * 1024):
+    hmac.update(iv)
+    for chunk, eof in Util._chunks(f, 64 * 1024):
         if eof:
             chunk = pad(chunk)
-        o.write(cipher.encrypt(chunk))
+        ochunk = cipher.encrypt(chunk)
+        o.write(ochunk)
+        hmac.update(ochunk)
+    o.write(hmac.digest())
     o.close()
-    if not cacheDir.move(checksum, checksum + ".bak"):
-        cacheDir.remove(checksum + ".enc")
-        raise Exception("Unable to move old version to backup: %s", checksum)
-    if not cacheDir.move(checksum + '.enc', checksum):
-        raise Exception("Unable to rename encrypted file: %s", checksum)
 
-def makeSig(checksum, regenerator, cacheDir):
-    s = cacheDir.open(checksum + ".sig", "wb+")
+def processFull(checksum, regenerator, cacheDir, nameMac, signature=True):
     i = regenerator.recoverChecksum(checksum)
-    librsync.signature(i, s)
+    sig = None
+    if signature:
+        output = cacheDir.open(checksum + ".sig", "wb+")
+        sig = librsync.SignatureJob(output)
 
-def encryptFiles(db, crypto, cacheDir):
+    data = i.read(16 * 1024)
+    while data:
+        nameMac.update(data)
+        if sig:
+            sig.step(data)
+        data = i.read(16 * 1024)
+
+def encryptFilesAtLevel(db, crypto, cacheDir, chainlength=0):
     logger = logging.getLogger('')
+    logger.info("Encrypting files which chainlength = %d", chainlength)
     conn = db.conn
     c = conn.cursor()
-    regenerator = Regenerate.Regenerator(cacheDir, db, crypto)
+    regenerator = Regenerator.Regenerator(cacheDir, db, crypto)
 
-    r = c.execute("SELECT Checksum FROM Checksums WHERE InitVector IS NULL AND IsFile = 1 ORDER BY CheckSum")
+    r = c.execute("SELECT Checksum FROM Checksums WHERE Encrypted = 0 AND IsFile = 1 AND ChainLength = :chainlength ORDER BY CheckSum", {"chainlength": chainlength})
     checksums = r.fetchall()
     c2 = conn.cursor()
     for row in checksums:
-        checksum = row[0]
-        if not cacheDir.exists(checksum + ".sig"):
-            logger.info("Making Signature for %s", checksum)
-            makeSig(checksum, regenerator, cacheDir)
-        logger.info("Encrypting %s", checksum)
-        iv = crypto.getIV()
-        cipher = crypto.getContentCipher(iv)
-        encryptFile(checksum, cacheDir, cipher, iv, crypto.pad)
-        biv = base64.b64encode(iv)
-        st = os.stat(cacheDir.path(checksum))
+        try:
+            checksum = row[0]
+            logger.info("Encrypting %s", checksum)
+            signature = not cacheDir.exists(checksum + ".sig")
+                
+            nameHmac = crypto.getHash()
+            processFull(checksum, regenerator, cacheDir, nameHmac, signature)
 
-        c2.execute('UPDATE CheckSums SET InitVector = ?, DiskSize = ? WHERE Checksum = ?', (biv, st.st_size, checksum))
-        conn.commit()
-        cacheDir.remove(checksum + '.bak')
+            iv = crypto.getIV()
+            cipher = crypto.getContentCipher(iv)
+            hmac = crypto.getHash(func=hashlib.sha512)
+            encryptFile(checksum, cacheDir, cipher, iv, crypto.pad, hmac, nameHmac)
+            #biv = base64.b64encode(iv)
+            newCks = nameHmac.hexdigest()
+
+            ost = os.stat(cacheDir.path(checksum))
+            st = os.stat(cacheDir.path(checksum + ".enc"))
+
+            logger.info("{} => {}: {} -> {}".format(checksum, newCks, ost.st_size, st.st_size))
+
+
+            c2.execute('UPDATE CheckSums SET Encrypted = 1, DiskSize = :size, Checksum = :newcks WHERE Checksum = :cks', {"size": st.st_size, "newcks": newCks, "cks": checksum})
+            c2.execute('UPDATE CheckSums SET Basis = :newcks WHERE Basis = :cks', {"newcks": newCks, "cks": checksum})
+
+            if not cacheDir.move(checksum, checksum + ".bak"):
+                cacheDir.remove(checksum + ".enc")
+                raise Exception("Unable to move old version to backup: {} -> {}".format(checksum, checksum + ".bak"))
+            if not cacheDir.move(checksum + '.enc', newCks):
+                raise Exception("Unable to rename encrypted file: {}".format(checksum))
+            if not cacheDir.move(checksum + ".sig", newCks + ".sig"):
+                raise Exception("Unable to rename signature file: {}".format(checksum + ".sig"))
+
+            conn.commit()
+            cacheDir.remove(checksum + '.bak')
+        except Exception as e:
+            conn.rollback()
+            logger.error("Unable to convert checksum: %s :: %s", checksum, e)
+            logger.exception(e)
+            raise e
+
+def encryptFiles(db, crypto, cacheDir):
+    conn = db.conn
+    r = conn.execute("SELECT MAX(ChainLength) FROM CheckSums")
+    z = r.fetchone()[0]
+    for level in range(z, -1, -1):
+        encryptFilesAtLevel(db, crypto, cacheDir, level)
+
+def generateSignatures(db, cacheDir):
+    r = c.execute("SELECT Checksum FROM Checksums")
+    regenerator = Regenerator.Regenerator(cacheDir, db, crypto)
+    for row in r.fetchall():
+        checksum = row[0]
+        logger = logging.getLogger('')
+        sigfile = checksum + '.sig'
+        if not cacheDir.exists(sigfile):
+            logger.info("Generating signature for {}".format(checksum))
+            makeSig(checksum, regenerator, cacheDir)
+
 
 def processArgs():
     parser = argparse.ArgumentParser(description='Encrypt the database')
@@ -75,11 +129,11 @@ def processArgs():
 
     parser.add_argument('--filenames',      dest='filenames', action='store_true', default=False,       help='Encrypt filenames. Default=%(default)s')
     parser.add_argument('--files',          dest='files',     action='store_true', default=False,       help='Encrypt files. Default=%(default)s')
-    parser.add_argument('--signatures',     dest='sigs',      action='store_true', default=False,       help='Generate signatures. Default=%(default)s')
+    #parser.add_argument('--signatures',     dest='sigs',      action='store_true', default=False,       help='Generate signatures. Default=%(default)s')
 
     passgroup= parser.add_argument_group("Password/Encryption specification options")
     pwgroup = passgroup.add_mutually_exclusive_group(required=True)
-    pwgroup.add_argument('--password', '-p',dest='password', default=None, nargs='?', const=True,       help='Encrypt files with this password')
+    pwgroup.add_argument('--password', '-P',dest='password', default=None, nargs='?', const=True,       help='Encrypt files with this password')
     pwgroup.add_argument('--password-file', dest='passwordfile', default=None,                          help='Read password from file')
     pwgroup.add_argument('--password-url',  dest='passwordurl', default=None,                           help='Retrieve password from the specified URL')
     pwgroup.add_argument('--password-prog', dest='passwordprog', default=None,                          help='Use the specified command to generate the password on stdout')
@@ -87,7 +141,7 @@ def processArgs():
     return parser.parse_args()
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger('')
     args = processArgs()
     password = Util.getPassword(args.password, args.passwordfile, args.passwordurl, args.passwordprog)
@@ -103,6 +157,8 @@ def main():
 
     cacheDir = CacheDir.CacheDir(os.path.join(args.database, args.client))
 
+    #if args.sigs:
+    #    generateSignatures(db, cacheDir)
     if args.filenames:
         encryptFilenames(db, crypto)
     if args.files:
