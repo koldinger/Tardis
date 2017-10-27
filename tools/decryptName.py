9ae9ae446e72292f06a6c2c1c2d882e541702def
Eric Koldinger <kolding@washington.edu>
2017-10-27 11:50:15 -0700

    More modificaions.  Works from command line now.

--- tools/decryptName.py
+++ tools/decryptName.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-from Tardis import Defaults, Util, TardisDB, TardisCrypto, CacheDir, librsync, Regenerator, Config
+from Tardis import Defaults, Util, TardisDB, TardisCrypto, CacheDir, librsync, Regenerator, Config, RemoteDB
 import sqlite3
 import argparse, logging
 import os.path
@@ -8,11 +8,52 @@ import os
 import sys
 import base64
 import hashlib
-import sys
 import progressbar
+import urlparse
 
 logger = None
 
+
+def getDB(client, database, crypt, password, new=False, allowRemote=True, allowUpgrade=False, dbdir=None, dbname=None, schema=None):
+    loc = urlparse.urlparse(database)
+    # This is basically the same code as in Util.setupDataConnection().  Should consider moving to it.
+    if (loc.scheme == 'http') or (loc.scheme == 'https'):
+        if not allowRemote:
+            raise Exception("This command cannot be executed remotely.  You must execute it on the server directly.")
+        # If no port specified, insert the port
+        if loc.port is None:
+            netloc = loc.netloc + ":" + Defaults.getDefault('TARDIS_REMOTE_PORT')
+            dbLoc = urlparse.urlunparse((loc.scheme, netloc, loc.path, loc.params, loc.query, loc.fragment))
+        else:
+            dbLoc = database
+        tardisdb = RemoteDB.RemoteDB(dbLoc, client)
+        cache = tardisdb
+    else:
+        basedir = os.path.join(database, client)
+        if not dbdir:
+            dbdir = os.path.join(database, client)
+        else:
+            dbdir = os.path.join(dbdir, client)
+        dbfile = os.path.join(dbdir, dbname)
+        if new and os.path.exists(dbfile):
+            raise Exception("Database for client %s already exists." % (client))
+
+        cache = CacheDir.CacheDir(basedir, 2, 2, create=new)
+        schema = schema if new else None
+        tardisdb = TardisDB.TardisDB(dbfile, backup=False, initialize=schema, allow_upgrade=allowUpgrade)
+
+    if password:
+        Util.authenticate(tardisdb, client, password)
+
+    return (tardisdb, cache)
+
+def reader(quiet):
+    prompt = '' if quiet else '--> '
+    try:
+        while True:
+            yield raw_input(prompt)
+    except EOFError:
+        return
 
 def processArgs():
     parser = argparse.ArgumentParser(description='encrypt or decrypt filenames', fromfile_prefix_chars='@', add_help=False)
@@ -20,6 +61,9 @@ def processArgs():
     (_, remaining) = Config.parseConfigOptions(parser)
     Config.addCommonOptions(parser)
     Config.addPasswordOptions(parser, addcrypt=False)
+
+    parser.add_argument('--encrypt', '-e', dest='encrypt', default=False, action='store_true', help='Encrypt names instead of decrypting')
+    parser.add_argument('--quiet', '-q', dest='quiet', default=False, action='store_true', help="Only print the translation, not the input strings")
 
     parser.add_argument('--help', '-h',     action='help');
     parser.add_argument('names',          nargs='*', help="List of pathnames to decrypt")
@@ -35,17 +79,27 @@ def main():
     args = processArgs()
     password = Util.getPassword(args.password, args.passwordfile, args.passwordprog)
 
-    crypto = TardisCrypto.TardisCrypto(password, args.client)
+    (_, _, crypto) = Util.setupDataConnection(args.database, args.client, password, args.keys, args.dbname, args.dbdir)
 
-    path = os.path.join(args.database, args.client, args.dbname)
-    db = TardisDB.TardisDB(path, backup=False)
+    data = args.names
+    if not data:
+        tty = os.isatty(0)
+        if not tty:
+            data = map(str.strip, sys.stdin.readlines())
+        else:
+            data = reader(args.quiet)
 
-    Util.authenticate(db, args.client, password)
-    (f, c) = db.getKeys()
-    crypto.setKeys(f, c)
+    for i in data:
+        if not args.quiet:
+            print i, " \t => \t",
+        try:
+            if (args.encrypt):
+                print crypto.encryptPath(i)
+            else:
+                print crypto.decryptPath(i)
+        except Exception as e:
+            print "Caught exception: " + str(e)
 
-    for i in args.names:
-        print "=>", crypto.decryptPath(i)
 
 if __name__ == "__main__":
     main()
