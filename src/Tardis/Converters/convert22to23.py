efa7d5a21d199ce7fb37b7e71073e254a6f1895c
Eric Koldinger <kolding@washington.edu>
2025-03-28 12:58:21 -0700

    Changes for using a virtual file system ID instead of a device ID.

---
+++ src/Tardis/Converters/convert22to23.py
@@ -0,0 +1,115 @@
+# vim: set et sw=4 sts=4 fileencoding=utf-8:
+#
+# Tardis: A Backup System
+# Copyright 2013-2024, Eric Koldinger, All Rights Reserved.
+# kolding@washington.edu
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holder nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import sqlite3
+import sys
+import logging
+import functools
+import os.path
+
+from . import convertutils
+from Tardis import Util
+
+version = 22
+
+@functools.cache
+def getName(conn, nameid):
+    c = conn.execute("SELECT Name FROM Names WHERE NameID = :nameid", {"nameid": nameid})
+    r = c.fetchone()
+    return r[0]
+
+def findpath(conn, row):
+    name = getName(conn, row[0])
+    if row[5] == 0 and row[6] == 0:
+        pName = "/"
+    else:
+        cur = conn.execute( "SELECT NameID, FirstSet, LastSet, Inode, Device, Parent, ParentDev FROM Files WHERE Inode = :inode AND Device = :device AND :firstset BETWEEN FirstSet AND LastSet", {"inode": row[5], "device": row[6], "firstset": row[1]})
+        parent = cur.fetchone()
+        pName = findpath(conn, parent)
+    return os.path.join(pName, name)
+
+@functools.cache
+def getDeviceId(conn, virtualDevice):
+    cur = conn.execute("SELECT DeviceID FROM Devices WHERE VirtualID = :virtualDev", {"virtualDev": virtualDevice})
+    row = cur.fetchone()
+    if row:
+        deviceId = row[0]
+    else:
+        c = conn.execute("INSERT INTO Devices (VirtualID) VALUES (:virtualDev)", {"virtualDev": virtualDevice})
+        deviceId = c.lastrowid
+    return deviceId
+
+def upgrade(conn, logger):
+    convertutils.checkVersion(conn, version, logger)
+
+    conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS Devices (
+            DeviceID    INTEGER PRIMARY KEY AUTOINCREMENT,
+            VirtualID   TEXT UNIQUE NOT NULL
+        )
+        """
+    )
+
+    conn.execute("ALTER TABLE Files ADD COLUMN DeviceID INTEGER REFERENCES Devices(DeviceID)")
+    conn.execute("ALTER TABLE Files ADD COLUMN ParentDevID INTEGER REFERENCES Devices(DeviceID)")
+
+    # Here we put the name insertion but it really doesn't work, because we really want to insert
+    # encrypted names.
+    cursor = conn.execute("SELECT DISTINCT(Device) FROM Files UNION SELECT DISTINCT(ParentDev) FROM Files")
+    rows = cursor.fetchall()
+
+    total = 0
+    for row in rows:
+        device = row[0]
+        deviceId = getDeviceId(conn, Util.hashPath(str(device)))
+        #name = row[0]
+        c = conn.execute("UPDATE Files SET DeviceID = :deviceid WHERE Device = :device", {"deviceid": deviceId, "device": device})
+        total += c.rowcount
+        c = conn.execute("UPDATE Files SET ParentDevId = :deviceid WHERE ParentDev = :device", {"deviceid": deviceId, "device": device})
+        total += c.rowcount
+    
+    print(f"Total rows updated: {total}")
+
+
+    convertutils.updateVersion(conn, version, logger)
+    conn.commit()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger('')
+
+    if len(sys.argv) > 1:
+        db = sys.argv[1]
+    else:
+        db = "tardis.db"
+
+    conn = sqlite3.connect(db)
+    upgrade(conn, logger)
